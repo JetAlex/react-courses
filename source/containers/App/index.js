1aa24708f05c4b34b6f1efaf6dc2a9dab0cedd21
alex.dyachenko <alex.dyachenko@omnigon.com>
2019-06-21 17:59:17 +0300

    lesson 4

--- source/containers/App/index.js
+++ source/containers/App/index.js
@@ -1,7 +1,8 @@
 // Core
 import React, {Component} from 'react';
-import { hot } from 'react-hot-loader';
+import {hot} from 'react-hot-loader';
 import Feed from "../../components/Feed";
+import {Provider} from '../../components/HOC/withProfile';
 
 import avatar from '../../theme/assets/lisa.png';
 
@@ -14,6 +15,10 @@ const options = {
 @hot(module)
 export default class App extends Component {
   render() {
-    return <Feed {...options}/>;
+    return (
+      <Provider value={ options }>
+        <Feed/>
+      </Provider>
+    )
   }
 }
