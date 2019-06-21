bfce0e68d762bdd81d6c52b93860c41ef5fe9a6d
alex.dyachenko <alex.dyachenko@omnigon.com>
2019-06-21 18:02:59 +0300

    Homework 4

--- source/components/Composer/index.js
+++ source/components/Composer/index.js
@@ -1,21 +1,24 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 import Styles from './styles.m.css';
 
+import {Consumer} from '../../components/HOC/withProfile';
+
 export default class Composer extends Component {
-  render () {
-
-    const {avatar, currentUserFirstName} = this.props;
-
+  render() {
     return (
-      <section className={ Styles.composer }>
-        <img src={ avatar } />
+      <Consumer>
+        { (context) => (
+          <section className={Styles.composer}>
+            <img src={context.avatar}/>
 
-        <form>
-          <textarea placeholder={ `What's on your mind, ${ currentUserFirstName }?` }/>
-          <input type='submit' value='Post'/>
-        </form>
-      </section>
+            <form>
+              <textarea placeholder={`What's on your mind, ${ context.currentUserFirstName }?`}/>
+              <input type='submit' value='Post'/>
+            </form>
+          </section>
+        )}
+      </Consumer>
     )
   }
 }
