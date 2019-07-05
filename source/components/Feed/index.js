48a62f83446def091252e2ea5cf75cfdf7ba56ae
alex.dyachenko <alex.dyachenko@omnigon.com>
2019-07-05 17:21:59 +0300

    Lesson 9

--- source/components/Feed/index.js
+++ source/components/Feed/index.js
@@ -1,4 +1,6 @@
 import React, {Component} from 'react';
+import {Transition} from 'react-transition-group';
+import {fromTo} from 'gsap';
 
 import Styles from "./styles.m.css";
 import StatusBar from "../StatusBar";
@@ -7,8 +9,9 @@ import Post from "../Post";
 import Spinner from "../Spinner";
 import Catcher from '../Catcher';
 import {withProfile} from '../../components/HOC/withProfile';
-import { api, TOKEN, GROUP_ID } from '../../config/api';
-import { socket } from '../../socket/init';
+import {api, TOKEN, GROUP_ID} from '../../config/api';
+import {socket} from '../../socket/init';
+import Postman from "../Postman";
 
 @withProfile
 export default class Feed extends Component {
@@ -18,37 +21,37 @@ export default class Feed extends Component {
   };
 
   componentDidMount() {
-    const { currentUserFirstName, currentUserLastName } = this.props;
+    const {currentUserFirstName, currentUserLastName} = this.props;
 
     this._fetchPosts();
     socket.emit('join', GROUP_ID);
 
     socket.on('create', (postJSON) => {
-      const { data: createdPost, meta } = JSON.parse(postJSON);
+      const {data: createdPost, meta} = JSON.parse(postJSON);
 
       if (`${currentUserFirstName} ${currentUserLastName}` !== `${meta.authorFirstName} ${meta.authorLastName}`) {
-        this.setState(({ posts }) => ({
+        this.setState(({posts}) => ({
           posts: [createdPost, ...posts],
         }));
       }
     });
 
     socket.on('remove', (postJSON) => {
-      const { data: removedPost, meta } = JSON.parse(postJSON);
+      const {data: removedPost, meta} = JSON.parse(postJSON);
 
       if (`${currentUserFirstName} ${currentUserLastName}` !== `${meta.authorFirstName} ${meta.authorLastName}`) {
-        this.setState(({ posts }) => ({
-          posts: posts.filter( post => post.id !== removedPost.id),
+        this.setState(({posts}) => ({
+          posts: posts.filter(post => post.id !== removedPost.id),
         }));
       }
     });
 
     socket.on('like', (postJSON) => {
-      const { data: likedPost, meta } = JSON.parse(postJSON);
+      const {data: likedPost, meta} = JSON.parse(postJSON);
 
       if (`${currentUserFirstName} ${currentUserLastName}` !== `${meta.authorFirstName} ${meta.authorLastName}`) {
-        this.setState(({ posts }) => ({
-          posts: [...posts.map( (post) => post.id === likedPost.id ? likedPost : post)],
+        this.setState(({posts}) => ({
+          posts: [...posts.map((post) => post.id === likedPost.id ? likedPost : post)],
         }));
       }
     });
@@ -73,7 +76,7 @@ export default class Feed extends Component {
       method: 'GET',
     });
 
-    const { data: posts } = await response.json();
+    const {data: posts} = await response.json();
 
     this.setState({
       posts,
@@ -90,10 +93,10 @@ export default class Feed extends Component {
         'Content-Type': 'application/json',
         Authorization: TOKEN,
       },
-      body: JSON.stringify({ comment }),
+      body: JSON.stringify({comment}),
     });
 
-    const { data: post } = await response.json();
+    const {data: post} = await response.json();
 
     this.setState(({posts}) => ({
       posts: [post, ...posts],
@@ -111,9 +114,9 @@ export default class Feed extends Component {
       },
     });
 
-    const { data: likedPost } = await response.json();
+    const {data: likedPost} = await response.json();
 
-    this.setState(({ posts}) => ({
+    this.setState(({posts}) => ({
       posts: posts.map(
         post => post.id === likedPost.id ? likedPost : post
       ),
@@ -131,10 +134,23 @@ export default class Feed extends Component {
       },
     });
 
-    this.setState(({ posts }) => ({
+    this.setState(({posts}) => ({
       posts: posts.filter((post) => post.id !== id),
       isPostsFetching: false,
     }))
+  };
+
+  _animateComposerEnter = (composer) => {
+    fromTo(composer, 1,
+      {
+        opacity: 0,
+        rotationX: 50,
+      },
+      {
+        opacity: 1,
+        rotationX: 0,
+      }
+    );
   };
 
   render() {
@@ -157,7 +173,15 @@ export default class Feed extends Component {
       <section className={Styles.feed}>
         <Spinner isPostsFetching={isPostsFetching}/>
         <StatusBar/>
-        <Composer _createPost={this._createPost}/>
+        <Transition
+          appear
+          in
+          timeout={1000}
+          onEnter={this._animateComposerEnter}
+        >
+          <Composer _createPost={this._createPost}/>
+        </Transition>
+        <Postman/>
         {postsJSX}
       </section>
     )
--- source/components/StatusBar/index.js
+++ source/components/StatusBar/index.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
+import {Transition} from 'react-transition-group';
+import {fromTo} from 'gsap';
 
 import Styles from './styles.m.css';
 import cx from 'classnames';
 
 import {withProfile} from '../../components/HOC/withProfile';
-import { socket } from '../../socket/init';
+import {socket} from '../../socket/init';
 
 @withProfile
 export default class StatusBar extends Component {
@@ -31,12 +33,16 @@ export default class StatusBar extends Component {
     socket.removeListener('disconnect');
   }
 
+  _animateStatusBarEnter = (statusBar) => {
+    fromTo(statusBar, 1, {opacity: 0}, {opacity: 1});
+  };
+
   render() {
 
     const {avatar, currentUserFirstName, currentUserLastName} = this.props;
-    const { online } = this.state;
+    const {online} = this.state;
 
-    const statusStyle = cx( Styles.status, {
+    const statusStyle = cx(Styles.status, {
       [Styles.online]: online,
       [Styles.offline]: !online,
     });
@@ -44,18 +50,25 @@ export default class StatusBar extends Component {
     const statusMessage = online ? 'Online' : 'Offline';
 
     return (
-      <section className={Styles.statusBar}>
-        <div className={ statusStyle }>
-          <div>{statusMessage}</div>
-          <span />
-        </div>
-        <button>
-          <img src={avatar}/>
-          <span>{currentUserFirstName}</span>
-          &nbsp;
-          <span>{currentUserLastName}</span>
-        </button>
-      </section>
+      <Transition
+        in
+        appear
+        timeout={1000}
+        onEnter={this._animateStatusBarEnter}
+      >
+        <section className={Styles.statusBar}>
+          <div className={statusStyle}>
+            <div>{statusMessage}</div>
+            <span/>
+          </div>
+          <button>
+            <img src={avatar}/>
+            <span>{currentUserFirstName}</span>
+            &nbsp;
+            <span>{currentUserLastName}</span>
+          </button>
+        </section>
+      </Transition>
     )
   }
 }
