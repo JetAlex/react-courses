29e64a88f18818271b0c0a9fe5c888c01888d05b
alex.dyachenko <alex.dyachenko@omnigon.com>
2019-07-05 18:17:22 +0300

    Homework 9

--- source/components/Postman/index.js
+++ source/components/Postman/index.js
@@ -1,14 +1,47 @@
 import React from 'react';
 
 import Styles from './styles.m.css';
-import { withProfile} from '../HOC/withProfile';
+import {withProfile} from '../HOC/withProfile';
+
+import {Transition} from 'react-transition-group';
+import {fromTo} from 'gsap';
 
 const Postman = (props) => {
+  const _animatePostmanEnter = (postman) => {
+    fromTo(
+      postman,
+      2,
+      {x: 1000},
+      {x: 0}
+    );
+  };
+  const _animatePostmanExit = (postman) => {
+    fromTo(
+      postman,
+      2,
+      {x: 0},
+      {x: 1000}
+    );
+  };
+
   return (
-    <section className={ Styles.postman}>
-      <img src={ props.avatar}/>
-      <span>Welcome online, {props.currentUserFirstName}</span>
-    </section>
+    <Transition
+      in
+      appear
+      timeout={
+        {
+          appear: 2000,
+          enter: 4000,
+        }
+      }
+      onEnter={_animatePostmanEnter}
+      onEntered={_animatePostmanExit}
+    >
+      <section className={Styles.postman}>
+        <img src={props.avatar}/>
+        <span>Welcome online, {props.currentUserFirstName}</span>
+      </section>
+    </Transition>
   )
 };
 
